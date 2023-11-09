e161a56f93255eb25eaf76d7608fd5fc4b914ace
znataniel <nrojaszavalla@frba.utn.edu.ar>
2023-11-09 20:29:26 -0300

    Wire javascript to html, allowing operations to occur

--- js/calc.js
+++ js/calc.js
@@ -1,24 +1,67 @@
-// OPERATIONS
-
 const add = (a, b) => a + b;
 const substract = (a, b) => add(a, b * -1);
 const mult = (a, b) => a * b;
 const divide = (a, b) => mult(a, 1 / b);
 
 function operate(a, op, b) {
-  switch (op) {
-    case "+":
-      return add(a, b);
-    case "-":
-      return substract(a, b);
-    case "x":
-      return mult(a, b);
-    case "/":
-      if (b) return divide(a, b);
-      else return "Math Error";
-  }
+  if (a != null && b != null) {
+    switch (op) {
+      case "+":
+        return add(a, b);
+      case "-":
+        return substract(a, b);
+      case "x":
+        return mult(a, b);
+      case "/":
+        b ? divide(a, b) : "Math ERROR";
+      default:
+        return "Syntax ERROR";
+    }
+  } else return "Syntax ERROR";
 }
 
-function drawButtons() {}
+function wipe() {
+  lOp = null;
+  rOp = null;
+  operation = null;
+  inputField.textContent = "";
+}
 
-console.log("el pepe");
+const buttons = document.querySelectorAll(".calcBtns button");
+const inputField = document.querySelector(".calcInput");
+// const resultField = document.querySelector(".calcResult");
+let lOp = null;
+let rOp = null;
+let operation = null;
+
+/* Add event listeners for each calculator button. */
+buttons.forEach((btn) => {
+  // Add event listeners for number buttons
+  if (!isNaN(btn.textContent)) {
+    btn.addEventListener("click", () => {
+      inputField.textContent += btn.textContent;
+    });
+  }
+  // Add event listener for C button
+  else if (btn.className == "btnClear") btn.addEventListener("click", wipe);
+  // Add event listener for = button
+  else if (btn.className == "btnResult") {
+    btn.addEventListener("click", () => {
+      rOp = Number(
+        inputField.textContent.slice(
+          inputField.textContent.indexOf(operation) + 1,
+        ),
+      );
+      inputField.textContent = operate(lOp, operation, rOp);
+      lOp = +inputField.textContent;
+    });
+  }
+  // Add event listener for operation buttons
+  else {
+    btn.addEventListener("click", () => {
+      lOp = +inputField.textContent;
+      operation = btn.textContent;
+      inputField.textContent += operation;
+    });
+  }
+});
